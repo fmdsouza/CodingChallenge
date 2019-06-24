2b8e63009d2698d1628b0fcd184fec160a8e9eb5
Freeda <freeda31@gmail.com>
2019-06-24 18:29:54 +0530

    added a testcase for delete operation

--- configure_object_store_test.py
+++ configure_object_store_test.py
@@ -22,13 +22,24 @@ class RestApiTest(unittest.TestCase):
     }
     ]
 
+    def test_delete_resource(self):
+        with app.app_context():
+            res = delete_resource(2)
+            data = json.loads(res.get_data(as_text=True))
+            self.assertEqual(data, {'result': True})
+    
     def test_get_resources(self):
+        result = [{
+        'id': 1,
+        'name': u'John Smith',
+        'email': u'john.smith@gmail.com', 
+        'phone': u'800-555-5669',
+        'address': u'969 High St., Atlantis VA 34075'}]
         
-
         with app.app_context():
             res = get_resources()
             data = json.loads(res.get_data(as_text=True))
-            self.assertEqual(data['resources'], self.sample_records)
+            self.assertEqual(data['resources'], result)
             
     def test_get_resource(self):
         with app.app_context():
